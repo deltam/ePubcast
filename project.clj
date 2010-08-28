dc1bdf2437a8944a03bdc25bbf4f030e89c506e6
deltam <deltam@gmail.com>
2010-08-28 18:11:17 +0900

    とりあえず成功。ざっくり

--- project.clj
+++ project.clj
@@ -1,7 +1,16 @@
 (defproject epubcast "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-                 [compojure "0.4.0-RC3"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [compojure "0.4.1"]
                  [hiccup "0.2.6"]
-                 [ring/ring "0.2.0"]])
+                 ]
+  :dev-dependencies [[ring/ring-core "0.2.5"]
+                     [ring/ring-devel "0.2.5"]
+                     [ring/ring-jetty-adapter "0.2.5"]
+                     [ring/ring-httpcore-adapter "0.2.5"]])
+;                 [ring/ring "0.2.5"]
+;                 [ring/ring-devel "0.2.5"]
+;                 [ring/ring-jetty-adapter "0.2.5"]
+;                 [ring/ring-httpcore-adapter "0.2.5"]])
+ 
--- src/epubcast/core.clj
+++ src/epubcast/core.clj
@@ -3,17 +3,19 @@
     (:use [compojure.core]
           [hiccup.core]
           [hiccup.form-helpers]
-          [ring.util.response :only (redirect)]
-          [ring.adapter.jetty :only (run-jetty)]
+          [ring.middleware [session :only (wrap-session)]
+           [file :only (wrap-file)]]
+          [ring.adapter jetty]
+          [ring.util.response :only (file-response)]
+          [ring.middleware params stacktrace file file-info session]
           [clojure.contrib.duck-streams :only (reader writer)]
           [clojure.contrib.seq-utils :only (rand-elt)]))
-
+  
 (defn html-doc [id body]
-  (html [:h1 "Trojure - tropy on clojure"]
+  (html [:h1 "ePubcast"]
         [:div {:align "right"}
-         [:a {:href "/new"} "Create "]
-         [:a {:href (str "/edit/" id)} "Edit "]
-         [:a {:href "/"} "Random"]]
+         [:a {:href "/epub"} "epub feeds "]
+         [:a {:href "/file/susu.epub"} "susu.epub"]]
         [:div {:align "left"}
          [:blockquote {:style "border-style:solid"}
           body
@@ -25,9 +27,47 @@
     (html-doc (entry :id)
               (html [:pre (entry :text)]))))
 
+(defn cdata [text]
+  (str "<![CDATA[" text "]]>"))
+
 (defroutes main-routes
   (GET "/" []
        (main-body {:id "010" :text "epubcast"}))
+  (GET "/epub" []
+       (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+            (html
+             [:rss {:xmlns:itunes "http://www.itunes.com/dtds/podcast-1.0.dtd" :version "2.0"}
+              [:channel
+               [:title "ePubcast exp"]
+               [:link "http://www.example.com/podcasts/everything/index.html"]
+               [:language "ja"]
+;               [:copyright "&#x2117; &amp; &#xA9; 2005 John Doe &amp; Family"]
+               [:itunes:subtitle (cdata "ePubをPodcastで配信するサーバの実験")]
+               [:itunes:author "deltam"]
+               [:itunes:summary (cdata "実験中デース")]
+               [:description (cdata "実験実験！！！")]
+               [:itunes:owner
+                [:itunes:name "deltam the YutoLisper"]
+                [:itunes:email "deltam@gmail.com"]]
+;               [:itunes:image {:href "http://example.com/podcasts/everything/AllAboutEverything.jpg"}]
+               [:itunes:category {:text "Technology"}
+                [:itunes:category {:text "Gadgets"}]]
+               [:itunes:category {:text "TV &amp; Film"}]
+               
+               [:item
+                [:title "SuSu"]
+                [:itunes:author "deltam"]
+                [:itunes:subtitle "little story in desert"]
+                [:itunes:summary (cdata "昔書いたラノベ処女作をさらす")]
+                [:enclosure {:url "http://localhost:8080/file/susu.epub"
+                             :length "89427"
+                             :type "application/epub+zip"}]
+                [:guid "http://localhost:8080/file/susu.epub"]
+                [:pubDate "Wed, 15 Jun 2005 19:00:00 GMT"]
+                [:itunes:duration "7:04"]
+                [:itunes:keywords "salt, pepper, shaker, exciting"]]]])))
+  (GET ["/file/:filename" :filename #".*"] [filename]
+       (file-response filename {:root "./static"}))
   (ANY "*" []
        {:status 404, :body "<h1>Page not found</h1>"}))
 
