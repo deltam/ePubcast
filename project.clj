c1136332afd8760be53acf1e17d913fac18f004b
deltam <deltam@gmail.com>
2010-10-06 11:32:58 +0900

    added new ePub file's item modified wrapping run-jetty, -main added library jars

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.4.1"]
-                 [hiccup "0.2.6"]]
-  :dev-dependencies [[ring/ring-core "0.2.5"]
-                     [ring/ring-devel "0.2.5"]
-                     [ring/ring-jetty-adapter "0.2.5"]
-                     [ring/ring-httpcore-adapter "0.2.5"]])
+                 [hiccup "0.2.6"]
+                 
+                 [ring/ring-core "0.2.5"]
+                 [ring/ring-devel "0.2.5"]
+                 [ring/ring-jetty-adapter "0.2.5"]
+                 [ring/ring-httpcore-adapter "0.2.5"]])
 ;                 [ring/ring "0.2.5"]
 ;                 [ring/ring-devel "0.2.5"]
 ;                 [ring/ring-jetty-adapter "0.2.5"]
--- src/epubcast/core.clj
+++ src/epubcast/core.clj
@@ -1,15 +1,15 @@
 (ns epubcast.core
   (:gen-class)
-    (:use [compojure.core]
-          [hiccup.core]
-          [hiccup.form-helpers]
-          [ring.middleware [session :only (wrap-session)]
-           [file :only (wrap-file)]]
-          [ring.adapter jetty]
-          [ring.util.response :only (file-response)]
-          [ring.middleware params stacktrace file file-info session]
-          [clojure.contrib.duck-streams :only (reader writer)]
-          [clojure.contrib.seq-utils :only (rand-elt)]))
+  (:use [compojure.core]
+        [hiccup.core]
+        [hiccup.form-helpers]
+        [ring.middleware [session :only (wrap-session)]
+         [file :only (wrap-file)]]
+        [ring.adapter jetty]
+        [ring.util.response :only (file-response)]
+        [ring.middleware params stacktrace file file-info session]
+        [clojure.contrib.duck-streams :only (reader writer)]
+        [clojure.contrib.seq-utils :only (rand-elt)]))
   
 (defn html-doc [id body]
   (html [:h1 "ePubcast"]
@@ -42,10 +42,10 @@
                [:link "http://www.example.com/podcasts/everything/index.html"]
                [:language "ja"]
 ;               [:copyright "&#x2117; &amp; &#xA9; 2005 John Doe &amp; Family"]
-               [:itunes:subtitle (cdata "ePubをPodcastで配信するサーバの実験")]
+               [:itunes:subtitle "ePubをPodcastで配信するサーバの実験"]
                [:itunes:author "deltam"]
-               [:itunes:summary (cdata "実験中デース")]
-               [:description (cdata "実験実験！！！")]
+               [:itunes:summary "実験中デース"]
+               [:description "実験実験！！！"]
                [:itunes:owner
                 [:itunes:name "deltam the YutoLisper"]
                 [:itunes:email "deltam@gmail.com"]]
@@ -55,21 +55,49 @@
                [:itunes:category {:text "TV &amp; Film"}]
                
                [:item
+                [:title "Susu for ePubcast"]
+                [:itunes:author "deltam"]
+                [:itunes:subtitle "little story in desert"]
+                [:itunes:summary "昔書いたラノベ処女作をさらす"]
+                [:enclosure {:url "http://localhost:8080/file/susu_plain.epub"
+                             :length "100549"
+                             :type "application/epub+zip"}]
+                [:guid "http://localhost:8080/file/susu_plain.epub"]
+                [:pubDate "Mon, 4 Oct 2010 19:00:00 GMT"]
+                [:itunes:duration "7:04"]
+                [:itunes:keywords "salt, pepper, shaker, exciting"]]
+
+               [:item
+                [:title "スウスウと砂漠と運び屋 on podcast"]
+                [:itunes:author "deltam"]
+                [:itunes:subtitle "little story in desert"]
+                [:itunes:summary "昔書いたラノベ処女作をさらす"]
+                [:enclosure {:url "http://localhost:8080/file/susu2.epub"
+                             :length "101041"
+                             :type "application/epub+zip"}]
+                [:guid "http://localhost:8080/file/susu2.epub"]
+                [:pubDate "Mon, 4 Oct 2010 19:00:00 GMT"]
+                [:itunes:duration "7:04"]
+                [:itunes:keywords "salt, pepper, shaker, exciting"]]
+
+               [:item
                 [:title "SuSu"]
                 [:itunes:author "deltam"]
                 [:itunes:subtitle "little story in desert"]
-                [:itunes:summary (cdata "昔書いたラノベ処女作をさらす")]
+                [:itunes:summary "昔書いたラノベ処女作をさらす"]
                 [:enclosure {:url "http://localhost:8080/file/susu.epub"
                              :length "89427"
                              :type "application/epub+zip"}]
                 [:guid "http://localhost:8080/file/susu.epub"]
                 [:pubDate "Wed, 15 Jun 2005 19:00:00 GMT"]
                 [:itunes:duration "7:04"]
-                [:itunes:keywords "salt, pepper, shaker, exciting"]]]])))
+                [:itunes:keywords "salt, pepper, shaker, exciting"]]
+               ]])))
   (GET ["/file/:filename" :filename #".*"] [filename]
        (file-response filename {:root "./static"}))
   (ANY "*" []
        {:status 404, :body "<h1>Page not found</h1>"}))
 
 
-(run-jetty main-routes {:port 8080})
+(defn -main [& args]
+  (run-jetty main-routes {:port 8080}))
